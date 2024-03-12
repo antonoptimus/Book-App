da75dc3cd2e9869012159e824c2bfa9970f95019
Anton Eliseev <anton_saintp@mail.ru>
2024-03-12 14:27:38 +0300

    header

---
+++ src/components/header/header.js
@@ -0,0 +1,33 @@
+import { DivComponent } from "../../views/main/div-component";
+import "./header.css";
+
+export class Header extends DivComponent {
+  constructor(appState) {
+    super();
+    this.appState = appState;
+  }
+
+  render() {
+    this.el.innerHTML = "";
+    this.el.classList.add("header");
+    this.el.innerHTML = `
+    <div>
+      <img src="/static/logo.svg" alt="Logotype" />
+    </div>
+    <div class="menu">
+      <a href="#" class="menu__item">
+        <img src="/static/search-black.svg" alt="Search icon" />
+        <p>Поиск книг</p>
+      </a>
+      <a href="#favorites" class="menu__item">
+        <img src="/static/favorites.svg" alt="Favorites icon" />
+        <p>Избранное</p>
+        <div class="menu__counter">
+          ${this.appState.favorites.length}
+        </div>
+      </a>
+    </div>
+    `;
+    return this.el;
+  }
+}
--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,8 +1,7 @@
-// Импортируем AbstractView из файла view.js и onChange из пакета on-change.
 import { AbstractView } from "../../common/view.js";
 import onChange from "on-change";
+import { Header } from "../../components/header/header.js";
 
-// Экспортируем класс MainView.
 export class MainView extends AbstractView {
   // Определяем состояние представления по умолчанию.
   state = {
@@ -37,11 +36,15 @@ export class MainView extends AbstractView {
   render() {
     // Создаём основной контейнер для представления.
     const main = document.createElement("div");
-    // Заполняем контейнер содержимым, например, информацией о количестве книг в списке избранных.
-    main.innerHTML = `Число книг: ${this.appState.favorites.length}`;
     // Очищаем содержимое представления.
     this.app.innerHTML = "";
     // Добавляем созданный контейнер в корневой элемент представления.
     this.app.append(main);
+    this.renderHeader();
+  }
+
+  renderHeader() {
+    const header = new Header(this.appState).render();
+    this.app.prepend(header);
   }
 }
