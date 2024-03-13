e921dc01507ce03d2e8d02afb2a9518a8930a537
Anton Eliseev <anton_saintp@mail.ru>
2024-03-13 14:07:16 +0300

    finish

--- src/components/cardList/cardList.js
+++ src/components/cardList/cardList.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import { DivComponent } from "../../common/div-component";
 import { Card } from "../card/card";
-=======
-import { DivComponent } from "../../views/main/div-component";
->>>>>>> 9c133a6f44191ab0a7a340dee5973d601edfb68f
 import "./cardList.css";
 
 export class CardList extends DivComponent {
@@ -15,7 +11,6 @@ export class CardList extends DivComponent {
 
   render() {
     if (this.parentState.loading) {
-<<<<<<< HEAD
       this.el.innerHTML = '<div class="card_list__loader">Загрузка...</div>';
       return this.el;
     }
@@ -25,15 +20,6 @@ export class CardList extends DivComponent {
     for (const card of this.parentState.list) {
       cardGrid.append(new Card(this.appState, card).render());
     }
-=======
-      this.el.innerHTML = '<div class="card_list__loader">Loading...</div>';
-      return this.el;
-    }
-    this.el.classList.add("card_list");
-    this.el.innerHTML = `
-      <h1>Найдено книг - ${this.parentState.list.length}</h1>
-    `;
->>>>>>> 9c133a6f44191ab0a7a340dee5973d601edfb68f
     return this.el;
   }
 }
